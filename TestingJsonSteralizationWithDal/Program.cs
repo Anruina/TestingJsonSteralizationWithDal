673a9d7414a0a0d3042290e0b19617fcebd7be50
Anruina <anruiamv@hotmail.com>
2024-04-15 12:08:23 +0200

    Add project files.

---
+++ TestingJsonSteralizationWithDal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestingJsonSteralizationWithDal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
